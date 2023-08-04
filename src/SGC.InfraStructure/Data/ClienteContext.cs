9651728397d8d320f584d427b9d77685e367548f
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-08-03 22:37:21 -0300

    feat: Create `ClienteContext` class

---
+++ src/SGC.InfraStructure/Data/ClienteContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGC.ApplicationCore.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGC.InfraStructure.Data
+{
+    public class ClienteContext : DbContext
+    {
+        public ClienteContext(DbContextOptions<ClienteContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>().ToTable("Tb_Cliente");
+        }
+    }
+}
