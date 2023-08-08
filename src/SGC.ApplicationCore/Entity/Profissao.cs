c8dfe6164c0cf70eed0b9d56a3a074cf068a08af
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-08-07 21:17:44 -0300

    feat: Add `Profissao` model class and its `Cliente` relation

---
+++ src/SGC.ApplicationCore/Entity/Profissao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGC.ApplicationCore.Entity
+{
+    public class Profissao
+    {
+        public Profissao()
+        {
+            
+        }
+
+        public int ProfissaoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string CBO { get; set; }
+        public ICollection<ProfissaoCliente> ProfissoesClientes { get; set; }
+    }
+}
---
+++ src/SGC.ApplicationCore/Entity/ProfissaoCliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGC.ApplicationCore.Entity
+{
+    public class ProfissaoCliente
+    {
+        public ProfissaoCliente()
+        {
+            
+        }
+
+        public int Id { get; set; }
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; }
+        public int ProfissaoId { get; set; }
+        public Profissao Profissao { get; set; }
+    }
+}
