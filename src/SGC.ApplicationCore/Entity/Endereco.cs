29dfb2a5266e21bb88fb6f0e34bfccce3b52fbc5
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-08-07 21:16:54 -0300

    feat: Add `Endereco` model class

---
+++ src/SGC.ApplicationCore/Entity/Endereco.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGC.ApplicationCore.Entity
+{
+    public class Endereco
+    {
+        public Endereco()
+        {
+            
+        }
+
+        public int EnderecoId { get; set; }
+        public string Logradouro { get; set; }
+        public string Bairro { get; set; }
+        public string CEP { get; set; }
+        public string Referencia { get; set; }
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; }
+    }
+}
