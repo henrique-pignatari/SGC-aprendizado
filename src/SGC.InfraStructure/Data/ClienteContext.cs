1b522e4e55042ce9ee717b8326e0d98c141b5f27
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-08-19 19:38:20 -0300

    feat: Configure new Entities Endereco, Menu, Profissao and ProfissaoCliente using Fluent API

--- src/SGC.InfraStructure/Data/ClienteContext.cs
+++ src/SGC.InfraStructure/Data/ClienteContext.cs
@@ -22,8 +22,20 @@ namespace SGC.InfraStructure.Data
         {
             modelBuilder.Entity<Cliente>().ToTable("Cliente");
             modelBuilder.Entity<Contato>().ToTable("Contato");
+            modelBuilder.Entity<Endereco>().ToTable("Endereco");
+            modelBuilder.Entity<Profissao>().ToTable("Profissao");
+            modelBuilder.Entity<ProfissaoCliente>().ToTable("ProfissaoCliente");
 
-            #region Configurações de cliente
+            #region Configurações de Cliente
+
+            modelBuilder.Entity<Cliente>().HasKey(c => c.ClienteId);
+
+            modelBuilder.Entity<Cliente>()
+                .HasMany(c => c.Contatos)
+                .WithOne(c => c.Cliente)
+                .HasForeignKey(c => c.ClienteId)
+                .HasPrincipalKey(c => c.ClienteId);
+
             modelBuilder.Entity<Cliente>().Property(e => e.CPF)
                 .HasColumnType("varchar(11)")
                 .IsRequired();
@@ -33,7 +45,14 @@ namespace SGC.InfraStructure.Data
                 .IsRequired();
             #endregion
 
-            #region Configurações de contato
+            #region Configurações de Contato
+
+            modelBuilder.Entity<Contato>()
+                .HasOne(c => c.Cliente)
+                .WithMany(c => c.Contatos)
+                .HasForeignKey(c => c.ClienteId)
+                .HasPrincipalKey(c => c.ClienteId);
+
             modelBuilder.Entity<Contato>().Property(e => e.Nome)
                 .HasColumnType("varchar(200)")
                 .IsRequired();
@@ -45,6 +64,61 @@ namespace SGC.InfraStructure.Data
             modelBuilder.Entity<Contato>().Property(e => e.Telefone)
                 .HasColumnType("varchar(15)");
             #endregion
+
+            #region Configurações de Profissao
+            modelBuilder.Entity<Profissao>().Property(p => p.Nome)
+                .HasColumnType("varchar(400)")
+                .IsRequired();
+
+            modelBuilder.Entity<Profissao>().Property(p => p.CBO)
+                .HasColumnType("varchar(10)")
+                .IsRequired();
+
+            modelBuilder.Entity<Profissao>().Property(p => p.Nome)
+                .HasColumnType("varchar(1000)")
+                .IsRequired();
+            #endregion
+
+            #region Configurações de Endereco
+            modelBuilder.Entity<Endereco>().Property(e => e.Bairro)
+                .HasColumnType("varchar(200)")
+                .IsRequired();
+
+            modelBuilder.Entity<Endereco>().Property(e => e.CEP)
+                .HasColumnType("varchar(15)")
+                .IsRequired();
+
+            modelBuilder.Entity<Endereco>().Property(e => e.Logradouro)
+                .HasColumnType("varchar(200)")
+                .IsRequired();
+
+            modelBuilder.Entity<Endereco>().Property(e => e.Referencia)
+                .HasColumnType("varchar(400)");
+            #endregion
+
+            #region Configurações de ProfissaoCliente
+
+            modelBuilder.Entity<ProfissaoCliente>().HasKey(c => c.Id);
+
+            modelBuilder.Entity<ProfissaoCliente>()
+                .HasOne(pc => pc.Cliente)
+                .WithMany(c => c.ProfissoesClientes)
+                .HasForeignKey(pc => pc.ClienteId);
+
+            modelBuilder.Entity<ProfissaoCliente>()
+                .HasOne(pc => pc.Profissao)
+                .WithMany(p => p.ProfissoesClientes)
+                .HasForeignKey(pc => pc.ProfissaoId);
+
+            #endregion
+
+            #region Configurações de Menu
+            modelBuilder.Entity<Menu>()
+                .HasMany(m => m.SubMenu)
+                .WithOne()
+                .HasForeignKey(m => m.MenuId);
+            #endregion
+
         }
     }
 }
