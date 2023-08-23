2487a9c70faa0526494802c4dc787e693d60281f
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-08-22 22:51:10 -0300

    feat: Add application core and Entity framework repositories

---
+++ src/SGC.ApplicationCore/Interfaces/Repository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGC.ApplicationCore.Interfaces.Repository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity Adicionar(TEntity entity);
+        void Atualizar(TEntity entity);
+        IEnumerable<TEntity> ObterTodos();
+        TEntity ObterPorId(int id);
+        IEnumerable<TEntity> Buscar(Expression<Func<TEntity, bool>> predicado);
+        void Remover(TEntity entity);
+
+    }
+}
